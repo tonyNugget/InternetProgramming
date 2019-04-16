01dcbd6d4966e14744fa7e66ccc34ead1a0fad3d
Anthony Zerka <azerka@csumb.edu>
2019-04-16 19:02:49 +0000

    plz

--- Lab07/signup.php
+++ Lab07/signup.php
@@ -68,6 +68,25 @@
       $username = "zerka";
       $password = "";
   
+  
+       //when connecting from Heroku
+
+    if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+        $host = $url["host"];
+
+        $dbname = substr($url["path"], 1);
+
+        $username = $url["user"];
+
+        $password = $url["pass"];
+
+    } 
+    
+  
+  
       // Get Data from DB
       try {
         
